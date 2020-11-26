a24047d51ca1bb785e43b34c9962c8b7e46b1168
John Lins <46113320+JohnLins@users.noreply.github.com>
2020-11-26 12:27:23 -0800

    Update NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,68 +1,94 @@
 import numpy as np
 
+e = np.exp(1)
+
 np.random.seed(1)
 
 synapticWeights = 2 * np.random.random((3, 1)) - 1
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+
+def softplus(x):
+    return np.log(1+((e)**x))
  
-def sigmoidDerivative(x):
-    return x * (1 - x)
+def softplusDerivative(x):
+    return 1/(1+((e)**-x))
 
 
-def base(inputs):
+def neuron(inputs):
     inputs = inputs.astype(float) 
-    return sigmoid(np.dot(inputs, synapticWeights))
+    return softplus(np.dot(inputs, synapticWeights))
 
 def train(trainingInputs, trainingOutputs, trainingIterations):
     global synapticWeights
     for iteration in range(trainingIterations):
         
-        output = base(trainingInputs)
+        output = neuron(trainingInputs)
         
         error = trainingOutputs - output
         
-        adjustments = np.dot(trainingInputs.T, error * sigmoidDerivative(output))
-
+        adjustments = np.dot(trainingInputs.T, error * softplusDerivative(output))
+        
         synapticWeights += adjustments
+
 
 
 print("Random starting weights: ")
 print(synapticWeights)
 
 
-trainingInputs = np.array([[0,0,1],
-                            [1,1,1],
-                            [1,0,1],
-                            [0,1,1]])
+trainingInputs = np.array([[0.1,0.5,0.1],
+                            [0.5,0.5,0.5],
+                            [0.5,0.1,0.5],
+                            [0.1,0.5,0.5]])
 
-trainingOutputs = np.array([[0,1,1,0]]).T
+trainingOutputs = np.array([[1,0.1,1,0.1]]).T
 
   
-train(trainingInputs, trainingOutputs, 10000)
+train(trainingInputs, trainingOutputs, 1000)
 
+
+
+#LET'S RUN IT!
 
 print("Weights after training: ")
 print(synapticWeights)
 print("-------------------")
 
-input1 = str(input("Input 1: "))
-input2 = str(input("Input 2: "))
-input3 = str(input("Input 3: "))
+
+inputs = [0.1,0.5,0.1]
+output = neuron(np.array(inputs))
+
+
+y = np.array([abs(1 - output[0]), abs(0.1 - output[0])])
+word = np.array(["Thing 1", "Thing 0.1"])
+
+
+
+print(y)
+
+smallest = y[0]
+index = 0
+for i in range(len(y)):
+  if y[i] < smallest:
+    smallest = min(smallest, y[i])
+    index = i
+
+print("smallest: ")
+print(smallest)
+
+print("output: ")
+print(word[index])
+
+
+
+
+
+
+
+
     
-print("Input data = ", input1, input2, input3)
-print("-------------------")
+#print("Input data = ", input1, input2, input3)
+#print("-------------------")
 print("Output data: ")
-print(base(np.array([input1, input2, input3])))
+print(output)
 
-"""
-input 1: 1
-input 2: 0
-input 3: 0
-
-Output:
-somthing close to 1
-
-The neural network learns that if there is a 1 in the first column, that the output should be 1
-"""
